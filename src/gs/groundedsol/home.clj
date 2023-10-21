31352de1abb5f6703295ba0b653c2b3457f966d0
GetBitOutdoors <ryan@getbitoutdoors.com>
2023-10-20 23:16:41 -0400

    start to integrate email code. currently disabled

--- src/gs/groundedsol/home.clj
+++ src/gs/groundedsol/home.clj
@@ -13,52 +13,89 @@
             [gs.pages.about]
             [gs.pages.notes]
             [gs.hiccup :as bhiccup]
+            [com.biffweb.impl.util :as butil]
+            [com.biffweb.impl.auth :as auth]
+            [com.biffweb.impl.xtdb :as bxt]
             [gs.build :as build]
             [lambdaisland.hiccup :as hiccup]
             [xtdb.api :as xt]))
 
 (def email-disabled-notice
-  [:div.text-sm.mt-3.bg-blue-100.rounded.p-2
+  [:div
    "Until you add API keys for Postmark and reCAPTCHA, we'll print your sign-up "
    "link to the console. See config.edn."])
 
+(defn passed-recaptcha? [{:keys [biff/secret biff.recaptcha/threshold params]
+                          :or {threshold 0.5}}]
+  (or (nil? (secret :recaptcha/secret-key))
+    (let [{:keys [success score]}
+          (:body
+           (http/post "https://www.google.com/recaptcha/api/siteverify"
+             {:form-params {:secret (secret :recaptcha/secret-key)
+                            :response (:g-recaptcha-response params)}
+              :as :json}))]
+      (and success (or (nil? score) (<= threshold score))))))
+
+
+
+(defn send-email! [{:keys [biff.auth/email-validator
+                          biff/send-email
+                          params]
+                   :as ctx}]
+  (let [email (butil/normalize-email (:email params))
+        url ""
+        user-id (atom nil)]
+    (cond
+      (not (passed-recaptcha? ctx))
+      {:success false :error "recaptcha"}
+
+      (not (email-validator ctx email))
+      {:success false :error "invalid-email"}
+
+      (not (send-email ctx
+             {:template :signin-link
+              :to email
+              :url url
+              :user-exists false}))
+      {:success false :error "send-failed"}
+
+      :else
+      {:success true :email email :user-id @user-id})))
+
+(defn send-email-handler [{:keys [biff.auth/single-opt-in
+                                 biff.auth/new-user-tx
+                                 biff/db
+                                 params]
+                          :as ctx}]
+  (let [{:keys [success error email user-id]} (auth/send-link! ctx)]
+    (when (and success single-opt-in (not user-id))
+      (bxt/submit-tx (assoc ctx :biff.xtdb/retry false) (new-user-tx ctx email)))
+    {:status 303
+     :headers {"location" (if success
+                            (str "/link-sent?email=" (:email params))
+                            (str (:on-error params "/") "?error=" error))}}))
+
 (defn contact-form [{:keys [recaptcha/site-key params]}]
   (bhiccup/form
-    {:id "signup"
-     :action "/auth/send-link"
-     :hidden {:on-error "/"}
-     :class "sm:max-w-xs w-full"}
-    (biff/recaptcha-callback "submitSignup" "signup")
+    {:id "contact-form"
+     :action "/send-email"
+     :hidden {:on-error "/"}}
+    (biff/recaptcha-callback "submitContact" "contact-form")
     [:input#email
      {:name "email"
       :type "email"
-      :autocomplete "email"
-      :placeholder "Enter your email address"
-      :class '[border
-               border-gray-300
-               rounded
-               w-full
-               focus:border-teal-600
-               focus:ring-teal-600]}]
-    [:div.h-3]
+      :autocomplete "email"`
+      :placeholder "Enter your email address"}]
     [:button
      (merge (when site-key
               {:data-sitekey site-key
                :data-callback "submitSignup"})
        {:type "submit"
-        :class '[bg-teal-600
-                 hover:bg-teal-800
-                 text-white
-                 py-2
-                 px-4
-                 rounded
-                 w-full
-                 g-recaptcha]})
-     "Join the waitlist"]
+        :class '[g-recaptcha]})
+     "Contact"]
     (when-some [error (:error params)]
       [:<>
-       [:div.h-1]
-       [:div.text-sm.text-red-600
+       [:div
         (case error
           "recaptcha" (str "You failed the recaptcha test. Try again, "
                         "and make sure you aren't blocking scripts from Google.")
@@ -69,21 +106,11 @@
 
 (defn contact-page [ctx]
   (ui/page
-    (assoc ctx ::ui/recaptcha true)
-    [:div.bg-orange-50.flex.flex-col.flex-grow.items-center.p-3
-     [:div.h-12.grow]
-     [:img.w-40 {:src "/img/eel.svg"}]
-     [:div.h-6]
-     [:div.text-2xl.sm:text-3xl.font-semibold.sm:text-center.w-full
-      "The world's finest discussion platform"]
-     [:div.h-2]
-     [:div.sm:text-lg.sm:text-center.w-full
-      "Communities, channels, messages, even RSS—eelchat has it all. Coming soon."]
-     [:div.h-6]
-     (contact-form ctx)
-     [:div.h-12 {:class "grow-[2]"}]
-     [:div.text-sm biff/recaptcha-disclosure]
-     [:div.h-6]]))
+    (assoc ctx ::ui/recaptcha false)
+    [:div.
+     #_(contact-form ctx)
+     #_[:div biff/recaptcha-disclosure]
+     ]))
 
 (defn confirmation-page [{:keys [params] :as ctx}]
   (ui/page
@@ -91,23 +118,29 @@
    [:h2.text-xl.font-bold "Check your inbox"]
    [:p "We've sent a sign-in link to " [:span.font-bold (:email params)] "."]))
 
+(defn foo [request]
+  {:status 200
+   :headers {"content-type" "text/plain"}
+   :body "foo response"})
 
 (do
   (def plugin
     {:routes
      (vec
        (concat
-         [["" {:middleware [mid/wrap-redirect-signed-in]}
+         [["/foo" {:det foo}]
+          ["/send-email"          {:post send-email-handler}]
+          ["" {:middleware [mid/wrap-redirect-signed-in]}
            ["/"                  {:get gs.pages.index/home-page}]]
           ["/contact-confirmed"  {:get confirmation-page}]
-          ["/contact"             {:get contact-page}]
+          #_["/contact"             {:get contact-page}]
           ]
          (->>
            {:notes gs.pages.notes/page-hiccup
             :florida-plants gs.pages.fl-plants/page-hiccup
             :services gs.pages.services/page-hiccup
             :consultation gs.pages.consultation/page-hiccup
-            ;; :contact gs.pages.contact/page-hiccup
+            :contact gs.pages.contact/page-hiccup
             :about gs.pages.about/page-hiccup}
            (mapv
              (fn [[page-key page-hiccup]]
